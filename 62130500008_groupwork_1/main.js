b839d5709ac8e6b8dbe44bb474bb76eb1c06ab36
Kritsanapon Rakkiatngam <kritsanapon.melo@mail.kmutt.ac.th>
2021-01-30 02:08:42 +0700

    [In Progressing] index.html -Track Tutorial [Del]images folder

--- 62130500008_groupwork_1/main.js
+++ 62130500008_groupwork_1/main.js
@@ -8,30 +8,30 @@
     }
     Vue.createApp(app).mount('#app');
 
-    let elements =document.getElementsByClassName("hover");
+    // let elements =document.getElementsByClassName("hover");
 
-    let changeOnHover = function() {
-        this.style.color='#17b5be'
-    };
-    let changeOnOut = function() {
-        this.style.color='black'
-    };
+    // let changeOnHover = function() {
+    //     this.style.color='#17b5be'
+    // };
+    // let changeOnOut = function() {
+    //     this.style.color='black'
+    // };
 
-    for (var i = 0; i < elements.length; i++) {
-        elements[i].addEventListener('mouseover', changeOnHover);
-        elements[i].addEventListener('mouseout', changeOnOut);
-    }
+    // for (var i = 0; i < elements.length; i++) {
+    //     elements[i].addEventListener('mouseover', changeOnHover);
+    //     elements[i].addEventListener('mouseout', changeOnOut);
+    // }
 
-    let elements2 =document.getElementsByClassName("hover2");
+    // let elements2 =document.getElementsByClassName("hover2");
 
-    let changeOnHover2 = function() {
-        this.style.backgroundColor = '#109198';
-    };
-    let changeOnOut2 = function() {
-        this.style.backgroundColor = '#17b5be';
-    };
+    // let changeOnHover2 = function() {
+    //     this.style.backgroundColor = '#109198';
+    // };
+    // let changeOnOut2 = function() {
+    //     this.style.backgroundColor = '#17b5be';
+    // };
 
-    for (var i = 0; i < elements2.length; i++) {
-        elements2[i].addEventListener('mouseover', changeOnHover2);
-        elements2[i].addEventListener('mouseout', changeOnOut2);
-    }
+    // for (var i = 0; i < elements2.length; i++) {
+    //     elements2[i].addEventListener('mouseover', changeOnHover2);
+    //     elements2[i].addEventListener('mouseout', changeOnOut2);
+    // }

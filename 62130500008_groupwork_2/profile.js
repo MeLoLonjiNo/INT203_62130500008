e217521921a2d39c1451e8418fd0c52510e6c362
Kritsanapon Rakkiatngam <kritsanapon.melo@mail.kmutt.ac.th>
2021-02-03 11:43:16 +0700

    [Change to]lab2 profile

---
+++ 62130500008_groupwork_2/profile.js
@@ -0,0 +1,19 @@
+    const app = {
+        data() {
+            return {
+                profilepic :'/images/profile image.jpg',
+                name : 'Kritsanapon Rakkiatngam',
+                igtag : '@melolonjino',
+                description : 'IT Student, UX/UI Designer, Game Designer, and Gamer. DM for work inquires or call to 080 XXX XXXX',
+                tag1 : '#IT',
+                tag2 : '#UX/UI Designer',
+                tag3 : '#Game Designer',
+                tag4 : '#Gamer',
+                button1 : 'Message',
+                button2 : 'Follow',
+            }
+        },
+
+        
+    }
+    mountApp = Vue.createApp(app).mount('#app')
